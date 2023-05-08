663ae8393fcdd94928c0ae2d4edde8625c1e82a1
icer2020 <foxoon_lee@163.com>
2023-05-08 09:48:34 +0800

    add

--- point24.py
+++ point24.py
@@ -161,3 +161,4 @@ for cnt, i in enumerate(rst_dict, start=1):
 elapsed_time = time.time() - starttime
 print('Total {:} results find for input: {:} [Elapase time: {:.3f}s] '.format(len(rst_dict.keys()), u_in, elapsed_time))
 
+
